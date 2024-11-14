2a12e293ad9fc01fee24789a63af288a41ee13b4
ArthurL <arthur.lonfils@gmail.com>
2024-11-14 22:05:41 +0100

    end: ex2.10

--- DevWeb/exercices/ex.10-11-12/src/components/Header/index.tsx
+++ DevWeb/exercices/ex.10-11-12/src/components/Header/index.tsx
@@ -1,22 +1,24 @@
 import { Box, Container } from "@mui/material";
+import Navbar from "./Navbar";
 
 interface HeaderProps {
   urlLogo: string;
 }
 
-const Header = (props: HeaderProps) => {
-  //const theme = useTheme();
+const menuButtonNames = ["Home", "Movies", "Cinemas"];
 
+const Header = (props: HeaderProps) => {
   return (
     <Box>
-      <Container>
-        <Box sx={{ display: "flex"}}>
-          <Box>
-            <img src={props.urlLogo} alt="logo" style={{ width: "235px", height: "150px"}} />
-          </Box>
-
-          
+      <Container sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+        <Box sx={{width: "235px", height: "150px"}}>
+            <img
+              src={props.urlLogo}
+              alt="logo"
+              style={{ width: "235px", height: "150px" }}
+            />
         </Box>
+        <Navbar menuButtonNames={menuButtonNames}/>
       </Container>
     </Box>
   );
