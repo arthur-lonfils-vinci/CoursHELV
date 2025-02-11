f9f85a38f38662b0ddf20f55c6f9488a9cd501d6
Vry0z <arthur.lonfils@gmail.com>
2025-02-11 15:09:43 +0100

    Update: SD2

--- SD-avance/Semaine1/ex4/src/ChoixOptions.java
+++ SD-avance/Semaine1/ex4/src/ChoixOptions.java
@@ -1,12 +1,11 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ChoixOptions {
 	
 	// associe le nom d'une option avec son objet Option correspondant
 	private Map<String, Option> options;
 	// ajouter ici les autres attributs
-	
+	private HashMap<Etudiant, LinkedList<String>> choixEtudiants;
 	
 	//constructeur prenant un entier et une suite de string en param�tres
 	//ces string repr�sentent les noms des diff�rentes options possibles
@@ -19,12 +18,20 @@ public class ChoixOptions {
 			options.put(nomOption, new Option(nomOption, nbEtudiantsParOption));
 		}
 		// initialiser les nouveaux attributs
+		choixEtudiants = new HashMap<Etudiant, LinkedList<String>>();
 	}
 
 	// cette m�thode encode les pr�f�rences des �tudiants
 	// il ne faut pas v�rifier que ces choix soient valides
 	public void ajouterPreferences(Etudiant etu, String choix1, String choix2,
 			String choix3) {
+        if (!choixEtudiants.containsKey(etu)) {
+			LinkedList<String> choix = new LinkedList<>();
+			choix.add(choix1);
+			choix.add(choix2);
+			choix.add(choix3);
+			choixEtudiants.put(etu, choix);
+		}
 	}
 
 	// cette m�thode est appel�e apr�s que les �tudiants aient donn� leurs pr�f�rences
@@ -34,6 +41,20 @@ public class ChoixOptions {
 	// il faut recourir au troisi�me choix.
 	// Cette m�thode doit faire appel � la m�thode inscrireEtudiant de la classe Option.
 	public void attribuerOptions() {
+		// trier les �tudiants par ordre d�croissant de moyenne
+		LinkedList<Etudiant> etudiants = new LinkedList<Etudiant>(choixEtudiants.keySet());
+		etudiants.sort((e1, e2) -> e2.getMoyenne() - e1.getMoyenne());
+
+		// attribuer les options aux �tudiants
+		for (Etudiant etu : etudiants) {
+			LinkedList<String> choix = choixEtudiants.get(etu);
+			for (String choixOption : choix) {
+				Option option = options.get(choixOption);
+				if (option.inscrireEtudiant(etu)) {
+					break;
+				}
+			}
+		}
 	}
 	
 	public String toString(){
--- SD-avance/Semaine1/ex6/src/GestionImpression.java
+++ SD-avance/Semaine1/ex6/src/GestionImpression.java
@@ -1,14 +1,23 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+
 public class GestionImpression {
-	
 
-	
-	public void ajouterImpression(Impression impr){
+    private HashMap<String, Impression> impressions;
 
-	}
-	
-	public Impression selectionnerImpression(){
+    public GestionImpression() {
+        this.impressions = new HashMap<String, Impression>();
+    }
 
-	}
+
+    public void ajouterImpression(Impression impr) {
+        impressions.put(impr.getIdUtilisateur(), impr);
+    }
+
+    public Impression selectionnerImpression() {
+        return impressions.remove(impressions.keySet().stream().findFirst().get());
+    }
 
 
 }
